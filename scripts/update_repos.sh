3d6f66d2a6598740197e67672bd36ad5ed48320a
Sakura286 <sakura286@outlook.com>
2024-04-12 08:39:50 +0800

    modify local path

--- scripts/update_repos.sh
+++ scripts/update_repos.sh
@@ -3,5 +3,5 @@
 # This script will run setup_repos.py on the locations where ros repos are expected on repos.ros.org
 # This is also useful when adding support for a new Ubuntu distro. 
 
-export PYTHONPATH=/home/rosbuild/reprepro_updater/src
-python /home/rosbuild/reprepro_updater/scripts/setup_repo.py /var/www/repos/ros_bootstrap -c
+export PYTHONPATH=/home/jenkins-agent/git/reprepro_updater/src
+python /home/jenkins-agent/git/reprepro_updater/scripts/setup_repo.py /var/www/repos/ros_bootstrap -c
